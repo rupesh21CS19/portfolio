2de6e8ab4a2fe64dd359a49db42d7b8a8e951fc3
Rupesh Kumar <138720635+rupesh21CS19@users.noreply.github.com>
2023-08-06 19:10:35 +0530

    index javascript

---
+++ index.js
@@ -0,0 +1,41 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true,
+});
+
+// mouseMove
+
+// function circleMouseOver() {
+//   window.addEventListener("mousemove", (event) => {
+//     document.querySelector(
+//       ".mini-circle"
+//     ).style.transform = `translate(${event.clientX}px,${event.clientY}px)`;
+//   });
+// }
+// circleMouseOver();
+
+// firstpage animation
+
+function firstPage() {
+  let v1 = gsap.timeline();
+
+  v1.from("nav", {
+    y: "-10",
+    opacity: 0,
+    duration: 1.1,
+    ease: Expo.easeInOut,
+  })
+    .to(".boundelm", {
+      y: "0",
+      duration: 1,
+      ease: Expo,
+      stagger: 0.2,
+    })
+    .from(".hero-footer", {
+      y: -10,
+      opacity: 0,
+      duration: 1,
+      ease: Expo.easeInOut,
+    });
+}
+firstPage();
